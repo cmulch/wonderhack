cf4db80d7d23a49f50e4f86baa223875aa95fc54
BuildTools <cwmulcah@mtu.edu>
2019-02-23 12:39:24 -0500

    Started pixel identification for encryption

--- plainsight.c
+++ plainsight.c
@@ -13,198 +13,27 @@
 unsigned short ReadLE2(FILE *fp);
 unsigned int ReadLE4(FILE *fp);
 
-/*
- * Read bitmap file header
- */
-BITMAPFILEHEADER *ReadBMFileHeader(FILE *fp)
+union {
+  unsigned int x;
+  struct {
+    unsigned int r:8;
+    unsigned int g:8;
+    unsigned int b:8;
+  } rgb;
+} pixel;
+
+unsigned char* encryptedBuffer(unsigned char* buffer, unsigned long fileLen)
 {
-    BITMAPFILEHEADER *header;
-    char           filetype[3] = {'\0', '\0', '\0'};
-    unsigned int   filesize;
-    unsigned short reserved1;
-    unsigned short reserved2;
-    unsigned long  offset;
+  for (int i = 56; i < fileLen + 1; i += 3)
+  {
+    pixel.rgb.r = (int)buffer[i];
+    pixel.rgb.g = (int)buffer[i + 1];
+    pixel.rgb.b = (int)buffer[i + 2];
 
-    /* File type (2 bytes) */
-    fread(&filetype, 1, 2, fp);
+    printf("%.2X%.2X%.2X ", pixel.rgb.r, pixel.rgb.g, pixel.rgb.b);
+  }
 
-    /* File size (4 bytes) */
-    filesize = (unsigned int) ReadLE4(fp);
-
-    /* Reserved 1 (2 bytes) */
-    fread(&reserved1, 2, 1, fp);
-
-    /* Reserved 2 (2 bytes) */
-    fread(&reserved2, 2, 1, fp);
-
-    /* Offset (4 bytes) */
-    offset = (unsigned long) ReadLE4(fp);
-
-    header = (BITMAPFILEHEADER *) malloc(sizeof(BITMAPFILEHEADER));
-    strcpy(header->bfType, filetype);
-    header->bfSize      = filesize;
-    header->bfReserved1 = reserved1;
-    header->bfReserved2 = reserved2;
-    header->bfOffBits   = offset;
-
-    return header;
-}
-
-/*
- * Returns size of information header
- */
-int SizeOfInformationHeader(FILE *fp)
-{
-    int headersize;
-    unsigned char buf[4];
-    int i;
-
-    fread(buf, 1, 4, fp);
-    for (i = 3; i >= 0; i--) {
-        headersize = (headersize << 8) | (int) buf[i];
-    }
-
-    fseek(fp, 14, SEEK_SET);
-
-    return headersize;
-}
-
-/*
- * Read bitmap core header (OS/2 bitmap)
- */
-BITMAPCOREHEADER *ReadBMCoreHeader(FILE *fp)
-{
-    BITMAPCOREHEADER *header;
-    unsigned int   headersize;
-    int            width;
-    int            height;
-    unsigned short planes;
-    unsigned short bitcount;
-
-    /* Header size (4 bytes) */
-    headersize = (unsigned int) ReadLE4(fp);
-
-    /* Width (2 bytes) */
-    width = (int) ReadLE2(fp);
-
-    /* Height (2 bytes) */
-    height = (int) ReadLE2(fp);
-
-    /* Planes (2 bytes) */
-    planes = (unsigned short) ReadLE2(fp);
-
-    /* Bit Count (2 bytes) */
-    bitcount = (unsigned short) ReadLE2(fp);
-
-    header = (BITMAPCOREHEADER *) malloc(sizeof(BITMAPCOREHEADER));
-    header->bcSize     = headersize;
-    header->bcWidth    = width;
-    header->bcHeight   = height;
-    header->bcPlanes   = planes;
-    header->bcBitCount = bitcount;
-
-    return header;
-}
-
-/*
- * Read bitmap info header (Windows bitmap)
- */
-BITMAPINFOHEADER *ReadBMInfoHeader(FILE *fp)
-{
-    BITMAPINFOHEADER *header;
-    unsigned int   headersize;
-    int            width;
-    int            height;
-    unsigned short planes;
-    unsigned short bitcount;
-    unsigned int   compression;
-    unsigned int   size_image;
-    int            x_pix_per_meter;
-    int            y_pix_per_meter;
-    unsigned int   clr_used;
-    unsigned int   clr_important;
-
-    /* Header size (4 bytes) */
-    headersize = (unsigned int) ReadLE4(fp);
-
-    /* Width (4 bytes) */
-    width = (int) ReadLE4(fp);
-
-    /* Height (4 bytes) */
-    height = (int) ReadLE4(fp);
-
-    /* Planes (2 bytes) */
-    planes = (unsigned short) ReadLE2(fp);
-
-    /* Bit Count (2 bytes) */
-    bitcount = (unsigned short) ReadLE2(fp);
-
-    /* Compression (4 bytes) */
-    compression = (unsigned int) ReadLE4(fp);
-
-    /* Size image (4 bytes) */
-    size_image = (unsigned int) ReadLE4(fp);
-
-    /* X pix per meter (4 bytes) */
-    x_pix_per_meter = (int) ReadLE4(fp);
-
-    /* Y pix per meter (4 bytes) */
-    y_pix_per_meter = (int) ReadLE4(fp);
-
-    /* Color used (4 bytes) */
-    clr_used = (unsigned int) ReadLE4(fp);
-
-    /* Color important (4 bytes) */
-    clr_important = (unsigned int) ReadLE4(fp);
-
-    header = (BITMAPINFOHEADER *) malloc(sizeof(BITMAPINFOHEADER));
-    header->biSize         = headersize;
-    header->biWidth        = width;
-    header->biHeight       = height;
-    header->biPlanes       = planes;
-    header->biBitCount     = bitcount;
-    header->biCompression  = compression;
-    header->biSizeImage    = size_image;
-    header->biXPixPerMeter = x_pix_per_meter;
-    header->biYPixPerMeter = y_pix_per_meter;
-    header->biClrUsed      = clr_used;
-    header->biClrImportant = clr_important;
-
-    return header;
-}
-
-/*
- * Read 2 bytes in little endian
- */
-unsigned short ReadLE2(FILE *fp)
-{
-    unsigned char buf[2];
-    unsigned short result = 0;
-    int i;
-
-    fread(buf, 1, 2, fp);
-    for (i = 1; i >= 0; i--) {
-        result = (result << 8) | (unsigned short) buf[i];
-    }
-
-    return result;
-}
-
-/*
- * Read 4 bytes in little endian
- */
-unsigned int ReadLE4(FILE *fp)
-{
-    unsigned char buf[4];
-    unsigned int result = 0;
-    int i;
-
-    fread(buf, 1, 4, fp);
-    for (i = 3; i >= 0; i--) {
-        result = (result << 8) | (unsigned int) buf[i];
-    }
-
-    return result;
+  return NULL;
 }
 
 void output(unsigned char *buffer, int fileLen)
@@ -381,6 +210,8 @@ int main(int argc,char **argv)
   }
 
   printf("Random Offset: %d\n", offset);
+
+  encryptedBuffer(buffer, fileLen);
 
   free(bmFileHeader);
   free(bmCoreHeader);
